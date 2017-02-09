83a6bc863ceaf7b5a52cc0398070af9d846ef53d
Sean <powerwaffe@gmail.com>
2017-02-08 23:19:49 -0500

    Changed everything and now displays Fahrenheit to Celsius

--- app/src/main/java/edu/dtcc/sean/porttoland/MainActivity.java
+++ app/src/main/java/edu/dtcc/sean/porttoland/MainActivity.java
@@ -2,11 +2,26 @@ package edu.dtcc.sean.porttoland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
+    // Define key for the bundle
+    private final String CELSIUS_KEY = "celsius value";
+
+    // Define variable for Celsius
+    private double celsius;
+
+    // Define input field
+    private EditText editText;
+
+    // Define the output field
+    private TextView tvDisplayCelsius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,20 +29,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Get references to the output fields
+        tvDisplayCelsius = (TextView) findViewById(R.id.tvDisplayCelsius);
+
+        // Get references to the input fields
+        editText = (EditText) findViewById(R.id.editText);
+
         // Create onClick listener
-        Button createString = (Button) findViewById(R.id.btnCreateString);
+        Button createString = (Button) findViewById(R.id.btnCalculateFahrenheit);
         createString.setOnClickListener(this);
+
+        if (savedInstanceState != null)
+        {
+            // Restore the save data
+            celsius = savedInstanceState.getDouble(CELSIUS_KEY);
+
+            tvDisplayCelsius.setText(Double.toString(celsius));
+            Log.d("SEAN", "bundle restored");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+
+        outState.putDouble(CELSIUS_KEY, celsius);
+
+        Log.d("SEAN", "bundle saved");
     }
 
     @Override
     public void onClick(View v)
     {
-        // Create textView objects
-        TextView textView1 = (TextView) findViewById(R.id.tvDisplayString);
-        TextView textView2 = (TextView) findViewById(R.id.editText);
+       try
+       {
+           // Read input
+           double fahrenheit = Double.parseDouble(editText.getText().toString());
 
-        // Set text from textView2 to textView1
-        textView1.setText(textView2.getText());
+           // Calculate
+           celsius = (fahrenheit - 32) * 5 / 9;
 
+           // Add results to textView
+           tvDisplayCelsius.setText(String.format("%.2f", celsius));
+       }
+       catch (Exception e)
+       {
+           // Display warning toast
+           Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_LONG).show();
+
+           // Clear fields
+           tvDisplayCelsius.setText("");
+       }
     }
 }
